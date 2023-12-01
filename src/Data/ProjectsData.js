c88d64e03c078e22f37079847daf6e51af7b2e55
mahdibenzid <etudiant.benzid.mahdi@uvt.tn>
2023-12-01 03:01:21 +0100

    CV REACT

---
+++ src/Data/ProjectsData.js
@@ -0,0 +1,11 @@
+const DUMMY_PROJECTS=[
+    {
+        projectTitle:'PFE',
+        description: 'pour lire le rapport, merci de click dessus .',
+        sourceLink:'https://www.youtube.com/watch?v=DDrqX4h9Mp4',
+        lastUpdated:'Janvier  2024'
+    },
+    
+];
+const ProjectsData={DUMMY_PROJECTS};
+export default ProjectsData;
